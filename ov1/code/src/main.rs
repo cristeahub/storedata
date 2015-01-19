e272359e5a1756f60aed15e29cff9700231b9bd7
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2015-01-19 02:45:30 +0100

    Rust setup for writing to file

---
+++ ov1/code/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::fs;
+use std::io::{File, IoResult, USER_RWX};
+
+fn write_to_file(f: &mut File,
+                 buff: [u8; 64 * 1024],
+                 size: isize) -> IoResult<()>{
+    f.write(&buff)
+}
+
+fn main() {
+    println!("Welcome to the beautiful write speed test program");
+    println!("This program will test the wiriting speeds of 2^n gb where n = [0..5]");
+
+    let sizes = [1, 2, 4, 8, 16, 32];
+    let buff = [0u8; 64 * 1024];
+
+    println!("Make temp dir");
+    match fs::mkdir(&Path::new("tmp"), USER_RWX) {
+        Err(why) => println!("! {:?}", why.kind),
+        Ok(_) => {},
+    };
+
+    println!("Create file");
+    let mut f = match File::create(&Path::new("tmp/a.txt")) {
+        Err(why) => panic!("Error! {}", why.desc),
+        Ok(file) => file,
+    };
+
+    println!("Write to file");
+    write_to_file(&mut f, buff, sizes[0]).unwrap_or_else(|why| {
+        panic!("Error! {}", why.desc)
+    });
+
+    println!("Done");
+}
